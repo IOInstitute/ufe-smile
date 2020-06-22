c574e5d6b9130beecd06bb58f79d38aae3fb2ef5
IO Institute <iotech.mn@gmail.com>
2020-06-22 12:44:48 +0800

    + initial commit

---
+++ UFE smile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UFE smile
+//
+//  Created by huuyee on 6/22/20.
+//  Copyright © 2020 IO Institute. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
